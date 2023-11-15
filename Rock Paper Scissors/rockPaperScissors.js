33d2dfad8f8d496b665446b69b4b64fe8951f2d3
Cat-McIlroy <148088736+Cat-McIlroy@users.noreply.github.com>
2023-11-15 17:14:57 +0000

    Moved CV files into CV folder, created Rock Paper Scissors
    All CV files moved into My CV folder, Week 8 Lab Rock, Paper, Scissors uploaded into it's own folder

---
+++ Rock Paper Scissors/rockPaperScissors.js
@@ -0,0 +1,54 @@
+
+
+function rps(){
+
+    console.log("working");
+
+var compGuess= Math.floor((Math.random() * 3) + 1);
+var userGuess= prompt("Please guess one of the following: Rock, Paper, Scissors:");
+
+if(userGuess.toLowerCase()=="rock"){
+    if(compGuess==1){
+        alert("You chose Rock, the Computer chose Rock \n\nYou draw!");
+    }
+    else if(compGuess==2){
+        alert("You chose Rock, the Computer chose Paper \n\nYou lose!");
+    }
+    else{
+        alert("You chose Rock, the Computer chose Scissors \n\nYou win!");
+    }
+}
+
+else if(userGuess.toLowerCase()=="paper"){
+    if(compGuess==1){
+        alert("You chose Paper, the Computer chose Rock \n\nYou win!");
+    }
+    else if(compGuess==2){
+        alert("You chose Paper, the Computer chose Paper \n\nYou draw!");
+    }
+    else{
+        alert("You chose Paper, the Computer chose Scissors \n\nYou lose!");
+    }
+}
+
+else if(userGuess.toLowerCase()=="scissors"){
+    if(compGuess==1){
+        alert("You chose Scissors, the Computer chose Rock \n\nYou lose!");
+    }
+    else if(compGuess==2){
+        alert("You chose Scissors, the Computer chose Paper \n\nYou win!");
+    }
+    else{
+        alert("You chose Scissors, the Computer chose Scissors \n\nYou draw!");
+    }
+}
+
+else{
+    alert("Invalid input, please try again!");
+}
+}
+
+document.getElementById("playButton").addEventListener("click", function(e){
+    rps();
+    event.preventDefault();
+});
