0def285e223b9162b3e2ab1945ceea1aaa9fe30c
Cat <148088736+Cat-McIlroy@users.noreply.github.com>
2025-02-25 21:48:56 +0000

    Delete Computer Science HDip/Project/Back-end Code directory
    delete old directory name

--- Computer Science HDip/Project/Back-end Code/medicines-shortages-assistant/src/main/java/com/cmcilroy/medicines_shortages_assistant/scrapers/WebScraper.java
+++
@@ -1,9 +0,0 @@
-package com.cmcilroy.medicines_shortages_assistant.scrapers;
-
-import java.util.List;
-
-public interface WebScraper {
-
-    List<String> scrapeUnavailableDrugs();
-
-}
--- Computer Science HDip/Project/Back-end Code/medicines-shortages-assistant/src/test/java/com/cmcilroy/medicines_shortages_assistant/cleaner/DatabaseCleaner.java
+++
@@ -1,7 +0,0 @@
-package com.cmcilroy.medicines_shortages_assistant.cleaner;
-
-public interface DatabaseCleaner {
-
-    void clearDatabase();
-
-}
